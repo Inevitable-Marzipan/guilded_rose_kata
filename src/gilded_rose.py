e34966a18156c4271ced8925eec667b72c40d2db
Tim Johnson <timothy.johnson@kainos.com>
2022-10-29 21:11:31 +0100

    refactors item

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from src.item import Item
 class GildedRose(object):
 
     def __init__(self, items):
@@ -36,11 +36,3 @@ class GildedRose(object):
                             item.quality = item.quality + 1
 
 
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
